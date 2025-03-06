df029892494a38d85a26d0d78149e488b151cc5a
enzB29 <enzo.bordet43@gmail.com>
2025-03-06 13:38:43 +0100

    All for events

--- proj/ProjetMusique/src/Controller/EventController.php
+++ proj/ProjetMusique/src/Controller/EventController.php
@@ -2,17 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\Artist;
+use App\Entity\Event;
+use App\Form\ArtistType;
+use App\Form\EventType;
+use App\Repository\ArtistRepository;
+use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class EventController extends AbstractController
 {
-    #[Route('/event', name: 'app_event')]
-    public function index(): Response
+    #[Route('/event/new', name: 'app_event_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $event = new Event();
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($event);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home'); // Rediriger après l'ajout
+        }
+
+        return $this->render('event/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/events', name: 'app_events_list')]
+    public function index(EventRepository $eventRepository): Response
+    {
+        $event = $eventRepository->findAll();
+
         return $this->render('event/index.html.twig', [
-            'controller_name' => 'EventController',
+            'events' => $event,
+        ]);
+    }
+
+
+    #[Route('/event/{id}', name: 'app_event_show')]
+    public function show(int $id, EventRepository $eventRepository): Response
+    {
+        $event = $eventRepository->find($id);
+
+        if (!$event) {
+            throw $this->createNotFoundException('Artiste non trouvé.');
+        }
+
+        return $this->render('event/show.html.twig', [
+            'event' => $event,
         ]);
     }
 }
