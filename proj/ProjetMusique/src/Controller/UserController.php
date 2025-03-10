b87ffe912066261493f0d976737632b51e23ee2d
Corentin75 <corentindetournay05@gmail.com>
2025-03-10 09:09:26 +0100

    access auth

--- proj/ProjetMusique/src/Controller/UserController.php
+++ proj/ProjetMusique/src/Controller/UserController.php
@@ -7,9 +7,12 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 final class UserController extends AbstractController
 {
+
     #[Route('/users', name: 'app_users_list')]
     public function listUsers(EntityManagerInterface $entityManager): Response
     {
